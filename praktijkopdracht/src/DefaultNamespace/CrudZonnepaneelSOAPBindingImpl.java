d7aaa8f0a62303ddeb42382ebc2b72a3e7434f61
T0mB <tom_bakker@quicknet.nl>
2017-03-11 16:27:16 +0100

    update voor update hah

---
+++ praktijkopdracht/src/DefaultNamespace/CrudZonnepaneelSOAPBindingImpl.java
@@ -0,0 +1,23 @@
+/**
+ * CrudZonnepaneelSOAPBindingImpl.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package DefaultNamespace;
+
+public class CrudZonnepaneelSOAPBindingImpl implements DefaultNamespace.CrudZonnepaneel_PortType{
+    public java.lang.String createZP(int serienummer, java.lang.String postcode) throws java.rmi.RemoteException {
+        return null;
+    }
+
+    public java.lang.String deleteZP(int serienummer, java.lang.String postcode) throws java.rmi.RemoteException {
+        return null;
+    }
+
+    public java.lang.String updateZP(java.lang.String serienummer, java.lang.String postcode) throws java.rmi.RemoteException {
+        return null;
+    }
+
+}
